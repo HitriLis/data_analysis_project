e8385bcf4243acc1b0f779a750785ebb2aa7aef6
Andrey <andrusha.kp@gmail.com>
2025-04-14 10:16:53 +0300

    fix notebooks

---
+++ data/load_data.py
@@ -0,0 +1,83 @@
+import os
+import json
+import time
+from clickhouse_connect import get_client
+import pandas as pd
+
+
+# Ждем, пока ClickHouse станет доступен
+def wait_for_clickhouse():
+    max_attempts = 10
+    # Чтение параметров подключения
+    CLICKHOUSE_HOST = os.getenv('CLICKHOUSE_HOST')
+    CLICKHOUSE_PORT = int(os.getenv('CLICKHOUSE_PORT'))
+    CLICKHOUSE_DB = os.getenv('CLICKHOUSE_DB')
+    CLICKHOUSE_USER = os.getenv('CLICKHOUSE_USER')
+    CLICKHOUSE_PASSWORD = os.getenv('CLICKHOUSE_PASSWORD')
+
+    for i in range(max_attempts):
+        try:
+            client = get_client(
+                host=CLICKHOUSE_HOST,
+                port=CLICKHOUSE_PORT,
+                database=CLICKHOUSE_DB,
+                username=CLICKHOUSE_USER,
+                password=CLICKHOUSE_PASSWORD
+            )
+            print("Подключение к ClickHouse установлено!")
+            return client
+        except Exception as e:
+            print(f"Ожидание ClickHouse... Попытка {i + 1}/{max_attempts}, ошибка: {e}")
+            time.sleep(2)
+    raise Exception("Не удалось подключиться к ClickHouse")
+
+
+def load_logins(client_db):
+    csv_path = "/home/jovyan/data/logins.csv"
+    exists_logins = bool(client_db.query("SELECT 1 FROM logins LIMIT 1").result_rows)
+    if exists_logins:
+        return "Данные уже есть в базе."
+    if not os.path.isfile(csv_path):
+        return f"Файл не найден: {csv_path}"
+    else:
+        try:
+            # Читаем CSV с помощью pandas
+            df = pd.read_csv(csv_path, parse_dates=['Timestamp'])
+            if 'Unnamed: 0' in df.columns:
+                df = df.drop(columns=['Unnamed: 0'])
+            client_db.insert_df('logins', df)
+        except Exception as e:
+            print(f"Ошибка при загрузке данных: {e}")
+            raise
+
+
+def load_users(client_db):
+    user_info_path = "/home/jovyan/data/user_info.json"
+    exists_user = bool(client_db.query("SELECT 1 FROM user_info LIMIT 1").result_rows)
+    if exists_user:
+        return "Данные уже есть в базе."
+    if not os.path.isfile(user_info_path):
+        return f"Файл не найден: {user_info_path}"
+    else:
+        with open(user_info_path, 'r', encoding='utf-8') as f:
+            user_info_data = json.load(f)
+        df = pd.DataFrame(user_info_data)
+        if not df.empty:
+            try:
+                client_db.insert(
+                    'user_info',
+                    df
+                )
+                return f"✅ Загружено {len(user_info_data)} записей в таблицу user_info."
+            except Exception as e:
+                return f"Ошибка при загрузке user_info.json: {e}"
+        else:
+            return "В файле нет данных для загрузки."
+
+
+if __name__ == "__main__":
+    print("Запуск скрипта загрузки данных...")
+    client = wait_for_clickhouse()
+    load_logins(client)
+    load_users(client)
+    print("Скрипт завершил выполнение")
---
+++ init/init.sql
@@ -0,0 +1,29 @@
+-- Создание базы данных (если она не существует)
+CREATE DATABASE IF NOT EXISTS analysis;
+
+-- Переключаемся на созданную базу данных
+USE analysis;
+
+-- Создание таблицы для пользователей
+CREATE TABLE IF NOT EXISTS user_info
+(
+    GUID String,
+    LastName Nullable(String),
+    FirstName Nullable(String),
+    MiddleName Nullable(String)
+)
+ENGINE = MergeTree()
+ORDER BY GUID;
+
+-- Создание таблицы для логинов
+CREATE TABLE IF NOT EXISTS logins
+(
+    Timestamp DateTime,
+    GUID String,
+    OuterIP String,
+    InnerIP String,
+    OpenVPNServer String,
+    NgToken String
+)
+ENGINE = MergeTree()
+ORDER BY Timestamp;
